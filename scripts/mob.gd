688a0546db71d5689b325e13ebf8130afb55c8d0
Allyson Jeronimo <allysonjeronimo@gmail.com>
2025-11-11 19:36:09 -0300

    Primeiro jogo finalizado

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,8 +1,11 @@
 extends RigidBody2D
 
+var speed = 0
+
 func _ready():
 	var animation_names = $AnimatedSprite2D.sprite_frames.get_animation_names()
 	var animation_index = randi() % animation_names.size()
+	
 	$AnimatedSprite2D.play(animation_names[animation_index])
 	
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
